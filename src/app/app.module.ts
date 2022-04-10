db233c9351a8c35092b736841fee24f132a2a4ca
L. Watkins <99104548+watkinswatkins@users.noreply.github.com>
2022-04-10 18:38:24 -0500

    Add files via upload
    just more components updated, more to work on still

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
+;
+import { LoginComponent } from './Login/login/login.component'import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 
+import {  }
+
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    LoginComponent
   ],
   imports: [
 
